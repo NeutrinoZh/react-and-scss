3114f38c7d4463a531bfa01b06a2e63f0e8f750f
NeutrinoZh <neutrinozh@gmail.com>
2022-08-21 16:45:21 +0300

    preload-animation

---
+++ pre-load-animation/src/Componets/App.js
@@ -0,0 +1,12 @@
+import '../SCSS/Componets/App.scss'
+import Preload from './Preload';
+
+function App() {
+  return (
+    <div className="app">
+        <Preload></Preload>
+    </div>
+  );
+}
+
+export default App;
---
+++ pre-load-animation/src/Componets/Preload.js
@@ -0,0 +1,15 @@
+import '../SCSS/Componets/Preload.scss'
+
+function Preload() {
+    return (
+        <div className='preload'>
+            <div className='preload-circle'></div>
+            <div className='preload-circle'></div>
+            <div className='preload-circle'></div>
+            <div className='preload-circle'></div>
+            <p>Loading...</p>
+        </div>
+    )
+}
+
+export default Preload
